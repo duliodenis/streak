7f484bd44aa88c07c65eb1263113de5439ba51a7
Dulio Denis <dulio.denis@yahoo.com>
2015-05-07 11:32:08 -0400

    Added the Add Streak VC

---
+++ Streak/Streak/ViewControllers/AddStreakViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddStreakViewController.h
+//  Streak
+//
+//  Created by Dulio Denis on 5/7/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddStreakViewController : UIViewController
+
+@end
---
+++ Streak/Streak/ViewControllers/AddStreakViewController.m
@@ -0,0 +1,27 @@
+//
+//  AddStreakViewController.m
+//  Streak
+//
+//  Created by Dulio Denis on 5/7/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+#import "AddStreakViewController.h"
+
+@interface AddStreakViewController ()
+
+@end
+
+@implementation AddStreakViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)save:(id)sender {
+    
+}
+
+
+
+@end
